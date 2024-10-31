1f54cfea6fb545694b7413a26a647603d12b8c51
mateomarta <163466552+mateomarta@users.noreply.github.com>
2024-10-31 19:28:00 -0300

    Add files via upload

---
+++ TP.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+def main():
+#INGRESO
+nombreApellido=int(input("Ingrese su nombre y apellido: "))
+dni=int(input("Ingrese su dni: "))
+opc=int(input("Por favor, ingrese 1 para cargar datos de clientes, 2 para ver listas o 3 para generar informe y salir"))
+
+if __name__ == "__main__":
+    main()
